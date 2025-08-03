512cc6593ce5bf1b006a6e86d7542b2ce3599c54
MrPancake24 <tymon.achcinski@gmail.com>
2025-08-03 17:31:13 +0200

    Update gravity.py

--- gravity.py
+++ gravity.py
@@ -5,10 +5,11 @@ def gravity(t, x, move):
         return t, move
     for i in reversed(range(5)):
         if t[x][i] != " " or i == 0:
-            if move % 2 == 0:
+            if move % 2 == 1:
                 t[x][i] = "X"
             else:
                 t[x][i] = "O"
             move += 1
             return t, move
+
 
